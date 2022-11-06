d4b7a3ebc5a419c5c9dddd4160a2613f1953ae75
Ana Rosario <arosario.mrd@gmail.com>
2022-11-06 10:10:38 -0500

    commit tree aa3d775dc0134b5af95a0964124a88e5358e1b23

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+        name: "Ana",
+        streetAddress: "2125 Amsterdam Ave",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const em2 ={...employee}
+   em2.name= "Sam"
+   em2.streetAddress= "11 Broadway"
+return em2
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam"
+    employee.streetAddress= "12 Broadway"
+
+    return employee
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    // Alternate using ES6 Spread operators:
+    // const newObj = { ...employee }
+    const newObj = Object.assign({}, employee);
+  
+    delete newObj[key];
+  
+    return newObj;
+
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
